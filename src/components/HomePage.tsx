8dbd20cbafb2f1ae294a8bf0acd4ce4636248a38
RudraNarayan2002 <brnsubudhi1151@gmail.com>
2026-01-18 22:55:03 +0530

    usetransition

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,13 @@
 import Counter from "./jotai/Counter";
 import ExampleUseEffectEvent from "./react-19-hooks/useEffectEvent/ExampleUseEffectEvent";
+import ExampleUseTransition from "./react-19-hooks/useTransition/ExampleUseTransition";
 
 const HomePage = () => {
   return (
     <div>
       {/* <Counter /> */}
-      <ExampleUseEffectEvent />
+      {/* <ExampleUseEffectEvent /> */}
+      <ExampleUseTransition />
     </div>
   );
 };
--- src/components/react-19-hooks/useTransition/ExampleUseTransition.tsx
+++ src/components/react-19-hooks/useTransition/ExampleUseTransition.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+import { useState, useTransition } from "react";
 
-const ExampleUseTransition = () => {
-  return <div>ExampleUseTransition</div>;
-};
+const bigList = Array.from({ length: 10000 }, (_, i) => `Item ${i}`);
 
-export default ExampleUseTransition;
+export default function ExampleUseTransition() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState(bigList);
+
+  const [isPending, startTransition] = useTransition();
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+
+    setQuery(value); // urgent UI change
+
+    startTransition(() => {
+      setResults(bigList.filter((item) => item.includes(value)));
+    });
+  }
+
+  return (
+    <>
+      <input value={query} onChange={handleChange} />
+      {isPending && <p>Updating results...</p>}
+      <ul>
+        {results.slice(0, 20).map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
